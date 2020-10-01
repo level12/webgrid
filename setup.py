2f58762e7850d3a3520dd895d4693b5fdcb6165d
Matt Lewellyn <guruofgentoo@gmail.com>
2020-10-01 16:00:18 -0400

    clarify removal of python 3.5 support

--- setup.py
+++ setup.py
@@ -55,7 +55,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
